fc2008503f99f3017e18351211f7e5c4d9df8d7f
Willamy Domingos <willamy.wlp@gmail.com>
2024-01-11 20:33:25 -0300

    Implement Login & CreateUser

--- db/CreateDatabase.sql
+++ db/CreateDatabase.sql
@@ -1,5 +1,15 @@
 USE mssupply_goods_api;
 
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(200) NOT NULL,
+    is_admin BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE goods (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(200) NOT NULL,
@@ -7,7 +17,9 @@ CREATE TABLE goods (
     manufacturer VARCHAR(200) NOT NULL,
     type VARCHAR(100) NOT NULL,
     description VARCHAR(1000) NOT NULL,
-    PRIMARY KEY (id)
+    user_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE goods_entries (
@@ -16,8 +28,10 @@ CREATE TABLE goods_entries (
     date DATETIME NOT NULL,
     location VARCHAR(200) NOT NULL,
     goods_id INT NOT NULL,
+    user_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (goods_id) REFERENCES goods(id)
+    FOREIGN KEY (goods_id) REFERENCES goods(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE goods_exit (
@@ -26,6 +40,8 @@ CREATE TABLE goods_exit (
     date DATETIME NOT NULL,
     location VARCHAR(200) NOT NULL,
     goods_id INT NOT NULL,
+    user_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (goods_id) REFERENCES goods(id)
+    FOREIGN KEY (goods_id) REFERENCES goods(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
--- main.py
+++ main.py
@@ -1,7 +1,96 @@
+import mysql.connector
 from flask import Flask, make_response, jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
+
+DATABASE = mysql.connector.connect(
+    host="localhost",
+    user="MainUser",
+    password="MainPassword",
+    database="mssupply_goods_api"
+)
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.config['JWT_SECRET_KEY'] = '9rhQbiX!1!MdORAbbAfP3ke0S4yTPLPJquKlpeejky-9Fpl3X0M2zggaRys4dR0r'
+jwt = JWTManager(app)
+
+
+
+# Verifica se o usuário é administador
+def user_is_admin(user_id):
+    cursor = DATABASE.cursor()
+    cursor.execute(f'SELECT is_admin FROM users WHERE id = {user_id}')
+    is_admin = cursor.fetchone()
+    cursor.close()
+
+    if is_admin:
+        return is_admin[0]
+    
+    else:
+        return None
+    
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if(not username or not password):
+        return jsonify({
+            "msg": "Faltam dados para concluir o login"
+        }), 401
+
+    cursor = DATABASE.cursor()
+    cursor.execute(f'SELECT * FROM users WHERE username = {username} AND password = {password}')
+    user = cursor.fetchone()
+    cursor.close()
+
+    if user is None:
+        return jsonify({
+            "msg": "Credenciais inválidas"
+        }), 401
+
+    access_token = create_access_token(identity=username)
+
+    return make_response(
+        jsonify(
+            messagem='Login realizado com sucesso',
+            access_token=access_token
+        ), 200
+    )
+
+@app.route('/api/v1/users', methods=['POST'])
+@jwt_required
+def create_user():
+    user = request.json
+    
+    if(not user):
+        return jsonify({
+            "msg": "Faltam dados para concluir o cadastro"
+        }), 401
+    
+    cursor = DATABASE.cursor()
+    cursor.execute(
+        f'SELECT * FROM users WHERE username = {user["username"]}'
+    )
+    user_is_exists = cursor.fetchone()
+    
+    if user_is_exists is not None:
+        return jsonify({
+            "msg": "Usuário já cadastrado!"
+        }), 401
+    
+    cursor.execute(
+        f"INSERT INTO users (name, email, username, password) VALUES ('{user['name']}', '{user['email']}', '{user['username']}', '{user['password']}')"
+    )
+
+    DATABASE.commit()
+    cursor.close()
+
+    return jsonify({
+        "msg": "Usuário criado com sucesso!"
+    }), 201
 
 
 if __name__ == '__main__':
