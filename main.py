13852b033fe35526938b8031fc06ea1dd9b82576
Willamy Domingos <willamy.wlp@gmail.com>
2024-01-11 22:49:02 -0300

    Validate Functions UPDATE-DELETE

--- main.py
+++ main.py
@@ -103,11 +103,21 @@ def get_users():
     cursor.execute(f"SELECT * FROM users")
     users = cursor.fetchall()
     cursor.close()
+    users_list = []
+
+    for user in users:
+        users_list.append({
+            "id": user[0],
+            "name": user[1],
+            "email": user[2],
+            "username": user[3],
+            "is_admin": user[5]
+        })
 
     return make_response(
         jsonify(
             msg='Lista de usuários',
-            users=users
+            users=users_list
         )
     )
 
@@ -131,9 +141,83 @@ def get_user(id):
     return make_response(
         jsonify(
             msg='Lista de usuários',
-            user=user
+            user= {
+                "id": user[0],
+                "name": user[1],
+                "email": user[2],
+                "username": user[3],
+                "is_admin": user[5]
+            }
         )
     )
+
+@app.route('/api/v1/users/<int:id>', methods=['PUT'])
+@jwt_required()
+def update_user(id):
+    user_id = get_jwt_identity()
+    is_admin = user_is_admin(user_id)
+
+    # Verifica se o usuário é administador ou se o id do usuário é o mesmo do token
+    if(is_admin[0] != id) and (is_admin[1] == 0):
+        return jsonify({
+            "msg": "Você não tem permissão para acessar esta rota"
+        }), 401
+
+    user = request.json
+
+    if(not user):
+        return jsonify({
+            "msg": "Faltam dados para concluir o cadastro"
+        }), 401
+
+    cursor = DATABASE.cursor()
+
+    # Verifica se o usuário é administador e se o campo is_admin foi enviado
+    if((is_admin[1] == 1) and ('is_admin' in user.keys())):
+        cursor.execute(
+            f"UPDATE users SET name='{user['name']}', email='{user['email']}', username='{user['username']}', password='{user['password']}', is_admin={user['is_admin']} WHERE id = {id}"
+        )
+        
+    else:
+        cursor.execute(
+            f"UPDATE users SET name='{user['name']}', email='{user['email']}', username='{user['username']}', password='{user['password']}' WHERE id = {id}"
+        )
+    
+    DATABASE.commit()
+    cursor.close()
+
+    return make_response(
+        jsonify(
+            msg='Usuário atualizado com sucesso',
+        )
+    )
+
+
+@app.route('/api/v1/users/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_user(id):
+    user_id = get_jwt_identity()
+    is_admin = user_is_admin(user_id)
+
+    # Verifica se o usuário é administador
+    if is_admin[1] == 0:
+        return jsonify({
+            "msg": "Você não tem permissão para acessar esta rota"
+        }), 401
+    
+    if is_admin[0] == id:
+        return jsonify({
+            "msg": "Você não pode deletar seu próprio usuário"
+        }), 401
+
+    cursor = DATABASE.cursor()
+    cursor.execute(f"DELETE FROM users WHERE id = {id}")
+    DATABASE.commit()
+    cursor.close()
+
+    return jsonify({
+        "msg": "Usuário deletado com sucesso!"
+    }), 201
 
 if __name__ == '__main__':
     app.run()
